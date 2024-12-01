03529df957d338b66d1eb1ffdcc98a9f99148211
Aneesha Kandala <aneeshakandala@Aneeshas-MacBook-Air.local>
2024-11-30 22:26:34 -0500

    got GUI layout to look similar to the example in Lab4 instruction. Now trying to apply encrypt and decrypt functionality

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -17,8 +19,11 @@ public class EnigmaFrame extends JFrame {
     // JLabel input = new JLabel("Input");
     // JLabel output = new JLabel("Output");
 
-    JButton encrypt;
-    JButton decrypt;
+    private JButton encrypt;
+    private JButton decrypt;
+
+    private JTextArea inputText;
+    private JTextArea outputText;
 
     private final Integer[] rotornums = {
         1, 2, 3, 4, 5
@@ -28,31 +33,48 @@ public class EnigmaFrame extends JFrame {
         //super();
         setTitle("Enigma GUI");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,400);
+        setSize(700,300);
         setLayout(new BorderLayout());
 
-        startingChar = new JTextField("EST", 5);
+        startingChar = new JTextField(3);//("EST", 5)
 
         in = new JComboBox<Integer>(rotornums); 
         mid = new JComboBox<Integer>(rotornums);
         outer = new JComboBox<Integer>(rotornums);
         //toValue.setEditable(false);
 
-        encrypt = new JButton ("Encrypt");
-        decrypt = new JButton ("Decrypt");
+        //three panels to make things look better
 
-        JPanel dpanel = new JPanel(new FlowLayout());
-        dpanel.add(new JLabel("Inner"));
-        dpanel.add(in);
-        dpanel.add(new JLabel("Middle"));
-        dpanel.add(mid);
-        dpanel.add(new JLabel("Out"));
-        dpanel.add(outer);
-        dpanel.add(new JLabel("Initial Positions"));
-        dpanel.add(startingChar);
-        dpanel.add(new JLabel("Input"));
-        dpanel.add(new JLabel("Output"));
+        JPanel upperPanel = new JPanel(new FlowLayout());
+        upperPanel.add(new JLabel("Inner"));
+        upperPanel.add(in);
+        upperPanel.add(new JLabel("Middle"));
+        upperPanel.add(mid);
+        upperPanel.add(new JLabel("Out"));
+        upperPanel.add(outer);
+    
+        upperPanel.add(new JLabel("Initial Positions"));
+        upperPanel.add(startingChar);
+       
 
+        encrypt = new JButton ("Encrypt");//needs event listener
+        decrypt = new JButton ("Decrypt");//needs event listener 
+        upperPanel.add(encrypt);
+        upperPanel.add(decrypt);
+
+        JPanel middlePanel = new JPanel(new FlowLayout());
+        middlePanel.add(new JLabel("Input"));
+        inputText = new JTextArea(5,30);
+        middlePanel.add(inputText);
+
+        JPanel bottomPanel = new JPanel(new FlowLayout());
+        bottomPanel.add(new JLabel("Output"));
+        outputText = new JTextArea(5,30);
+        bottomPanel.add(outputText);
+
+        add(upperPanel, BorderLayout.NORTH);
+        add(middlePanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
 
     }
 
